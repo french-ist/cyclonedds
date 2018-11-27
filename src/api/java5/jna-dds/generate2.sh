0956bbffa2b7d8b938109dfd1508d9ee8e9d5528
Jean Marline <jean.marline@adlinktech.com>
2018-11-27 18:12:54 +0100

    copy NativeSize.java locally + clean generated pom.xml

--- src/api/java5/jna-dds/generate2.sh
+++ src/api/java5/jna-dds/generate2.sh
@@ -5,6 +5,20 @@ rm -rf *tmp.h _jnaerator.choices  _jnaerator.macros.cpp  _jnaerator.normalizedPa
 src pom.xml _jnaerator.rawParsed.cpp
 cat ../../../core/ddsc/include/ddsc/ddsv2.h | sed '/#include "ddsc\/dds_public_impl.h"/ r ../../../core/ddsc/include/ddsc/dds_public_impl.h' | sed 's/#include "ddsc\/dds_public_impl.h"/\/\/#include "ddsc\/dds_public_impl.h"/g' > dds2tmp.h
 ./gen2.sh dds2tmp
+echo "=== patch src/main/java/org/eclipse/cyclonedds/ddsc/DdscLibrary.java"
+# comment line leading to "Caused by: java.lang.IllegalArgumentException: class [Lorg.eclipse.cyclonedds.ddsc.DdscLibrary$serdata; is not a supported argument type (in method dds_takecdr in class org.eclipse.cyclonedds.ddsc.DdscLibrary)"
+sed -i 's/public static native int dds_takecdr(DdscLibrary.dds_entity_t reader_or_condition, DdscLibrary.serdata buf/\/\/public static native int dds_takecdr(DdscLibrary.dds_entity_t reader_or_condition, DdscLibrary.serdata buf/g' src/main/java/org/eclipse/cyclonedds/ddsc/DdscLibrary.java
 # add dependency to jnaerator-runtime
-sed -i '/<dependencies>/a <dependency><groupId>com.nativelibs4java</groupId><artifactId>jnaerator-runtime</artifactId><version>0.13-SNAPSHOT</version></dependency>' pom.xml
+#sed -i '/<dependencies>/a <dependency><groupId>com.nativelibs4java</groupId><artifactId>jnaerator-runtime</artifactId><version>0.13-SNAPSHOT</version></dependency>' pom.xml
+echo "=== copy NativeSize.java"
+cp ~/00-Devel/JNAerator/jnaerator-runtime/src/main/java/com/ochafik/lang/jnaerator/runtime/NativeSize.java src/main/java/org/eclipse/cyclonedds/ddsc/
+echo "=== delete import of package com.ochafik.lang.jnaerator.runtime"
+cd src/main/java/org/eclipse/cyclonedds/ddsc
+for i in `ls`; do sed -i /ochafik/d $i;done
+echo "== change package of NativeSize.java"
+sed -i '1 i package org.eclipse.cyclonedds.ddsc;' NativeSize.java
+cd -
+echo "=== clean pom.xml"
+cat pom.xml | sed 106,112d |sed 43,70d | sed 23,31d > pom_tmp
+mv pom_tmp pom.xml
 mvn install
