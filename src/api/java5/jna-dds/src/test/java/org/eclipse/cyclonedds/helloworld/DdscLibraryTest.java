135ae169b690f881e54e38b5e76cfa1122b1dc34
Ndrianja Rakotomanga <ndrianja.rakotomanga@adlinktech.com>
2019-01-09 16:27:59 +0100

    FRCYC-68: Creating basic unit test

--- src/api/java5/jna-dds/src/test/java/org/eclipse/cyclonedds/helloworld/DdscLibraryTest.java
+++ src/api/java5/jna-dds/src/test/java/org/eclipse/cyclonedds/helloworld/DdscLibraryTest.java
@@ -18,155 +18,48 @@ public class DdscLibraryTest {
     int DDS_CHECK_EXIT = org.eclipse.cyclonedds.ddsc.dds_public_error.DdscLibrary.DDS_CHECK_EXIT;
     HelloWorldData_Helper helper = new HelloWorldData_Helper();
 
-    private class Publisher extends Thread {
+    @Test
+    public void writerCreationTest() {
+        /* Create a Participant. */
+        int part = DdscLibrary.dds_create_participant (0, null, null);
+        Assert.assertTrue(helper.dds_error_check(part, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
 
-        public Publisher() {}
+        /* Create a Topic. */
+        int topic = DdscLibrary.dds_create_topic(part, helper.getHelloWorldData_Msg_desc(), "HelloWorldData_Msg", null, null);
+        Assert.assertTrue(helper.dds_error_check(topic, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
 
-        public void run() {
-            /* Create a Participant. */
-            int part = DdscLibrary.dds_create_participant (0, null, null);
-            Assert.assertTrue(helper.dds_error_check(part, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
+        /* Create a Writer. */
+        int writer = DdscLibrary.dds_create_writer(part, topic, null, null);
+        Assert.assertTrue(writer > 0);
 
-            /* Create a Topic. */
-            int topic = DdscLibrary.dds_create_topic(part, helper.getHelloWorldData_Msg_desc(), "HelloWorldData_Msg", null, null);
-            Assert.assertTrue(helper.dds_error_check(topic, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
-
-            /* Create a Writer. */
-            int writer = DdscLibrary.dds_create_writer(part, topic, null, null);
-
-            System.out.println("=== [Publisher]  Waiting for a reader to be discovered ...\n");
-
-            DdscLibrary.dds_set_enabled_status(writer, DdscLibrary.DDS_PUBLICATION_MATCHED_STATUS);
-            Assert.assertTrue(helper.dds_error_check(topic, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0) ;
-
-            while (true) {
-                IntBuffer status = IntBuffer.allocate(Native.getNativeSize(Integer.class));
-                int ret = DdscLibrary.dds_get_status_changes(writer, status);
-                Assert.assertTrue(helper.dds_error_check(ret, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
-
-                if (status.get() == DdscLibrary.DDS_PUBLICATION_MATCHED_STATUS) {
-                    System.out.println("DDS_PUBLICATION_MATCHED_STATUS");
-                    break;
-                }
-
-                /* Polling sleep. */
-                int milliSeconds = 20;
-                org.eclipse.cyclonedds.ddsc.dds_public_time.DdscLibrary.dds_sleepfor(milliSeconds*1000000L);
-            }
-
-            /* Create a message to write. */
-            HelloWorldData_Msg.ByReference msg = new HelloWorldData_Msg.ByReference();
-            msg.userID = 1;
-            msg.message = helper.stringToPointer("Hello World!");
-            msg.write();
-            System.out.println("=== [Publisher]  Writing : ");
-            System.out.println("Message (" + msg.userID + ", " + msg.message.getString(0) + ")");
-
-            int ret = DdscLibrary.dds_write(writer, msg.getPointer());
-            Assert.assertTrue(helper.dds_error_check(ret, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
-
-            /* Deleting the participant will delete all its children recursively as well. */
-            ret = DdscLibrary.dds_delete(part);
-        }
+        /* Deleting the participant will delete all its children recursively as well. */
+        int ret = DdscLibrary.dds_delete(part);
     }
-
-    private class Subscriber extends Thread {
-
-        public void run(){
-            /* Create a Participant. */
-            int part = DdscLibrary.dds_create_participant (0, null, null);
-            Assert.assertTrue(helper.dds_error_check(part, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
-
-            /* Create a Topic. */
-            int topic = DdscLibrary.dds_create_topic(part,
-                helper.getHelloWorldData_Msg_desc(), "HelloWorldData_Msg", null, null);
-            Assert.assertTrue(helper.dds_error_check(topic, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
-
-            /* Create a reliable Reader. */
-            org.eclipse.cyclonedds.ddsc.dds_public_qos.DdscLibrary.dds_qos_t qos = org.eclipse.cyclonedds.ddsc.dds_public_qos.DdscLibrary.dds_qos_create();
-            org.eclipse.cyclonedds.ddsc.dds_public_qos.DdscLibrary.dds_qset_reliability(qos,
-                    org.eclipse.cyclonedds.ddsc.dds_public_qos.DdscLibrary.dds_reliability_kind.DDS_RELIABILITY_RELIABLE,
-                    10 * 1000000000); // DDS_SECS (10)
-
-			int reader = DdscLibrary.dds_create_reader (part, topic, qos, null);
-            Assert.assertTrue(helper.dds_error_check(reader, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
-
-            org.eclipse.cyclonedds.ddsc.dds_public_qos.DdscLibrary.dds_qos_delete(qos);
-
-            System.out.println("\n=== [Subscriber] Waiting for a sample ...");
-            
-            /*
-             * Initialize sample buffer, by pointing the void pointer within
-             * the buffer array to a valid sample memory location. */
-            //samples[0] = HelloWorldData_Msg__alloc ();
-            Pointer samplesAlloc = org.eclipse.cyclonedds.ddsc.dds_public_alloc.DdscLibrary
-                .dds_alloc(helper.getNativeSize("HelloWorldData_Msg"));
-            PointerByReference samplePtr = new PointerByReference(samplesAlloc);
-            dds_sample_info.ByReference infosPtr = new  dds_sample_info.ByReference();
-            dds_sample_info[] infosArr = (dds_sample_info[]) infosPtr.toArray(1);
-            
-            while(true){               
-                int readReturn = DdscLibrary.dds_read(reader, samplePtr, infosPtr, new NativeSize(1), 1);
-                Assert.assertTrue(helper.dds_error_check(readReturn, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
-                HelloWorldData_Msg  arrayMsgRef = new HelloWorldData_Msg(samplePtr.getValue());
-                arrayMsgRef.read();
-                infosPtr.read();
-                
-                //Check if we read some data and it is valid                
-                if (readReturn > 0 && infosArr[0].getValid_data() > 0)
-                {
-                    //Print Message
-                    HelloWorldData_Msg[] msgArray = (HelloWorldData_Msg[])arrayMsgRef.toArray(1);                    
-                    System.err.println("=== [Subscriber] Received : ");
-                    System.err.println("Message ("+ msgArray[0].userID+","+ msgArray[0].message.getString(0)+")");
-                    break;
-                }
-                else
-                {
-                    //Polling sleep
-                    int milliSeconds = 20;
-                    org.eclipse.cyclonedds.ddsc.dds_public_time.DdscLibrary.dds_sleepfor(milliSeconds*1000000L);
-                }
-            }
-
-            /* Free the data location. */
-            org.eclipse.cyclonedds.ddsc.dds_public_alloc.DdscLibrary.dds_sample_free(
-                samplesAlloc, 
-                helper.getHelloWorldData_Msg_desc(),
-                org.eclipse.cyclonedds.ddsc.dds_public_alloc.DdscLibrary.dds_free_op_t.DDS_FREE_ALL);
-
-            /* Deleting the participant will delete all its children recursively as well. */
-            int deleteStatus = DdscLibrary.dds_delete(part);
-            Assert.assertTrue(helper.dds_error_check(deleteStatus, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
-        }
-    }
-
 
     @Test
-    public void helloWorldTest() {
-        try {            
-            System.out.println("STARTING PUBLISHER");
-            Publisher p = new Publisher();
-            p.start();
-            sleep(1000);
+    public void readerCreationTest(){
+        /* Create a Participant. */
+        int part = DdscLibrary.dds_create_participant (0, null, null);
+        Assert.assertTrue(helper.dds_error_check(part, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
 
-            System.out.println("STARTING SUBSCRIBER");
-            Subscriber s = new Subscriber();
-            s.start();
+        /* Create a Topic. */
+        int topic = DdscLibrary.dds_create_topic(part,
+            helper.getHelloWorldData_Msg_desc(), "HelloWorldData_Msg", null, null);
+        Assert.assertTrue(helper.dds_error_check(topic, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
 
-            sleep(3000);
+        /* Create a reliable Reader. */
+        org.eclipse.cyclonedds.ddsc.dds_public_qos.DdscLibrary.dds_qos_t qos = org.eclipse.cyclonedds.ddsc.dds_public_qos.DdscLibrary.dds_qos_create();
+        org.eclipse.cyclonedds.ddsc.dds_public_qos.DdscLibrary.dds_qset_reliability(qos,
+                org.eclipse.cyclonedds.ddsc.dds_public_qos.DdscLibrary.dds_reliability_kind.DDS_RELIABILITY_RELIABLE,
+                10 * 1000000000); // DDS_SECS (10)
+        int reader = DdscLibrary.dds_create_reader (part, topic, qos, null);
+        Assert.assertTrue(helper.dds_error_check(reader, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
 
-            System.out.println("END");            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+        /* Delete qos */
+        org.eclipse.cyclonedds.ddsc.dds_public_qos.DdscLibrary.dds_qos_delete(qos);
 
-    public void sleep(int s){
-        try {
-            Thread.sleep(s);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        /* Deleting the participant will delete all its children recursively as well. */
+        int deleteStatus = DdscLibrary.dds_delete(part);
+        Assert.assertTrue(helper.dds_error_check(deleteStatus, DDS_CHECK_REPORT | DDS_CHECK_EXIT) > 0);
     }
 }
