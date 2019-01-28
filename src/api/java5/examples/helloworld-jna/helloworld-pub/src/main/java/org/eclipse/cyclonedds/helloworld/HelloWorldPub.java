65ea5cdae359ee0ada89e323d2fb2ee87c76f0e7
Ndrianja Rakotomanga <ndrianja.rakotomanga@adlinktech.com>
2019-01-28 14:40:46 +0100

    FRCYC-69: Fixing imports

--- src/api/java5/examples/helloworld-jna/helloworld-pub/src/main/java/org/eclipse/cyclonedds/helloworld/HelloWorldPub.java
+++ src/api/java5/examples/helloworld-jna/helloworld-pub/src/main/java/org/eclipse/cyclonedds/helloworld/HelloWorldPub.java
@@ -2,13 +2,9 @@ package org.eclipse.cyclonedds.helloworld;
 
 import java.nio.IntBuffer;
 
-import com.sun.jna.Native;
-import com.sun.jna.Pointer;
-import com.sun.jna.ptr.PointerByReference;
-
 import org.eclipse.cyclonedds.ddsc.dds.DdscLibrary;
-import org.eclipse.cyclonedds.ddsc.dds.dds_sample_info;
-import org.eclipse.cyclonedds.helper.NativeSize;
+
+import com.sun.jna.Native;
 
 /**
  * Hello world!
--- src/api/java5/examples/helloworld-jna/helloworld-sub/src/main/java/org/eclipse/cyclonedds/helloworld/HelloWorldSub.java
+++ src/api/java5/examples/helloworld-jna/helloworld-sub/src/main/java/org/eclipse/cyclonedds/helloworld/HelloWorldSub.java
@@ -1,14 +1,11 @@
 package org.eclipse.cyclonedds.helloworld;
 
-import java.nio.IntBuffer;
-
-import com.sun.jna.Native;
-import com.sun.jna.Pointer;
-import com.sun.jna.ptr.PointerByReference;
-
 import org.eclipse.cyclonedds.ddsc.dds.DdscLibrary;
 import org.eclipse.cyclonedds.ddsc.dds.dds_sample_info;
 import org.eclipse.cyclonedds.helper.NativeSize;
+
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.PointerByReference;
 
 /**
  * Hello world!
