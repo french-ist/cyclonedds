e7bbca78f991ab4ffd7b2557ada898a7e6304d34
JM <jean.marline@adlinktech.com>
2019-03-21 18:11:48 +0100

    FRCYC-85 Use the same header as C code for Java files - samples cyclone-dds java-generator-helper Done

--- src/api/java5/java-generator-helper/src/main/antlr4/org/eclipse/cyclonedds/.antlr/DdscLexer.java
+++ src/api/java5/java-generator-helper/src/main/antlr4/org/eclipse/cyclonedds/.antlr/DdscLexer.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright(c) 2006 to 2019 ADLINK Technology Limited and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Eclipse Distribution License
+ * v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ */
 // Generated from /home/ndrianja/IST/cyclonedds/src/api/java5/java-generator-helper/src/main/antlr4/org/eclipse/cyclonedds/Ddsc.g4 by ANTLR 4.7.1
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
